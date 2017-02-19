33f50f928fecfb94f94833fb8010cfdd1d46878e
Ketan <ketantoo@gmail.com>
2017-02-20 02:59:18 +0530

    Facebook ID Finder script

---
+++ find_facebook_id.py
@@ -0,0 +1,10 @@
+#author:Ketan D Ramteke
+#https://www.facebook.com/ketan.ramteke.794
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.facebook.com/ketan.ramteke.794"
+r = requests.get(url)
+soup = BeautifulSoup(r.content)
+str = soup.findAll("a",{"class":"uiButton uiButtonSpecial uiButtonLarge"})
+txt = unicode.join(u'\n',map(unicode,str))
+print txt[(txt.find("_id=")+4):(txt.find("&"))]
---
+++ score.py
@@ -0,0 +1,20 @@
+import random
+import requests
+import time
+from bs4 import BeautifulSoup
+
+str1 = ""
+url = "http://www.espncricinfo.com/india-v-england-2016-17/engine/match/1034829.html"
+#change the value of url to the live cricket match which you want to track
+while True:
+	r = requests.get(url)
+	soup = BeautifulSoup(r.content)
+	str2 = soup.find("title").text
+	if(str2 != str1):
+		print "******************"
+		print str2
+		str1 = str2
+		time.sleep(random.randint(30,200))
+		#sleep function is used to delay refresh rate of website.
+        	
+   
